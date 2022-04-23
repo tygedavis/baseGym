a63d65a41c1851f9d331cdb92beabd991bfeb776
Tyge Davis <tyge.davis@gmail.com>
2022-04-23 09:51:47 -0600

    Renamed reserved env vars

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,12 +6,12 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
+    apiKey: process.env.FB_API_KEY,
     authDomain: process.env.AUTH_DOMAIN_KEY,
     projectId: process.env.PROJ_ID,
     storageBucket: process.env.STORAGE_BUCKET,
     messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
+    appId: process.env.FB_APP_ID,
     measurementId: process.env.MEASUREMENT_ID
   };
 
