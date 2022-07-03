d615dbac41d63583b230d9d23a516f750932ff51
Tyge Davis <tyge.davis@gmail.com>
2022-07-03 17:37:56 -0600

    Adding lint rules for the front end

--- public/App.js
+++ public/App.js
@@ -1,27 +1,27 @@
-import React from "react";
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import SignUp from './views/SignUp';
 
 export default function App() {
-    const [ isLoggedIn, onChangeLoggedIn ] = React.useState(false);
-    const [ currentUser, onChangeCurrentUser ] = React.useState({});
+	const [ isLoggedIn, onChangeLoggedIn ] = React.useState(false);
+	const [, onChangeCurrentUser ] = React.useState({});
 
-  	if (isLoggedIn) {
-    	return (
-        	<View style={styles.container}>
-            	<Text>Open up App.js to start working on your app!</Text>
-            	<StatusBar style="auto" />
-        	</View>
-      	)
-    } else { 
-    	return (
-        	<SignUp
+	if (isLoggedIn) {
+		return (
+			<View style={styles.container}>
+				<Text>Open up App.js to start working on your app!</Text>
+				<StatusBar style="auto" />
+			</View>
+		);
+	} else { 
+		return (
+			<SignUp
 				onChangeLoggedIn={onChangeLoggedIn}
 				onChangeCurrentUser={onChangeCurrentUser}
 			/>
-        )
-    }
+		);
+	}
 }
     
 const styles = StyleSheet.create({
